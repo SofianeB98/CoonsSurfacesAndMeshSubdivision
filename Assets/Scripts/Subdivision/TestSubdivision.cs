1fd41b01711e13e0e0cb1f4cc0782cc6976d184f
Sofiane <sofiane.benembareck@hotmail.com>
2021-06-16 10:21:31 +0200

    On peut choisir dans l'inspector le type de subdivision

--- Assets/Scripts/Subdivision/TestSubdivision.cs
+++ Assets/Scripts/Subdivision/TestSubdivision.cs
@@ -5,8 +5,18 @@ using UnityEngine;
 
 namespace Subdivision
 {
+
+    public enum SubdiviserType
+    {
+        LOOP,
+        CATMULLCLARK,
+        BUTTERFLY
+    }
+
     public class TestSubdivision : MonoBehaviour
     {
+        public SubdiviserType type;
+
         private ISubdiviser subdiviser;
         [SerializeField] private MeshFilter meshFilter;
         private Mesh originalMesh;
@@ -16,21 +26,32 @@ namespace Subdivision
             originalMesh = meshFilter.mesh;
             //this.subdiviser = new LoopSubdivisionSurface(SubdivisionUtils.Weld(meshFilter.mesh, float.Epsilon, meshFilter.mesh.bounds.size.x), iterations);
             //this.subdiviser = new CatmullClark(SubdivisionUtils.Weld(meshFilter.mesh, float.Epsilon, meshFilter.mesh.bounds.size.x), iterations);
-            this.subdiviser = new Bitterfly(SubdivisionUtils.Weld(meshFilter.mesh, float.Epsilon, meshFilter.mesh.bounds.size.x), iterations);
+            //this.subdiviser = new Bitterfly(SubdivisionUtils.Weld(meshFilter.mesh, float.Epsilon, meshFilter.mesh.bounds.size.x), iterations);
            
-            this.meshFilter.mesh = subdiviser.GetMesh();
-
+            //this.meshFilter.mesh = subdiviser.GetMesh();
         }
 
         private void OnGUI()
         {
-            if (GUILayout.Button("Subdivide"))
+            if (GUILayout.Button("Subdivide", GUILayout.Width(150), GUILayout.Height(50)))
             {
+                switch (type)
+                {
+                    case SubdiviserType.LOOP:
+                        this.subdiviser = new LoopSubdivisionSurface(SubdivisionUtils.Weld(meshFilter.mesh, float.Epsilon, meshFilter.mesh.bounds.size.x), iterations);
+                        break;
+                    case SubdiviserType.CATMULLCLARK:
+                        this.subdiviser = new CatmullClark(SubdivisionUtils.Weld(meshFilter.mesh, float.Epsilon, meshFilter.mesh.bounds.size.x), iterations);
+                        break;
+                    case SubdiviserType.BUTTERFLY:
+                        this.subdiviser = new Bitterfly(SubdivisionUtils.Weld(meshFilter.mesh, float.Epsilon, meshFilter.mesh.bounds.size.x), iterations);
+                        break;
+                }
                 this.subdiviser.Subdivide(iterations);
                 this.meshFilter.mesh = this.subdiviser.GetMesh();
             }
 
-            if (GUILayout.Button("Reset Original"))
+            if (GUILayout.Button("Reset Original", GUILayout.Width(150), GUILayout.Height(50)))
             {
                 this.meshFilter.mesh = originalMesh;
             }
